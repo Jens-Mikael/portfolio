b3a4c3ce86a322b4ca418b59609bb8b9d85f8279
Jens-Mikael <jensmikael.stjernberg@gmail.com>
2023-06-05 15:44:58 +0200

    got the metadata working

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,11 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Providers from "@/components/Providers";
 
+export const metadata = {
+  title: "Portfolio",
+  description: "A portfolio built by Jens",
+};
+
 const sourceSansPro = Source_Sans_Pro({
   subsets: ["latin"],
   weight: ["300", "400", "600", "700"],
